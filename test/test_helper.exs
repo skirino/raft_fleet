68a30643786df6d4cd8cd1b56ce6f9cfdbc9c251
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2016-07-04 14:39:57 +0900

    Use slave module instead of ct_slave

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -22,15 +22,14 @@ defmodule SlaveNode do
   end
 
   def start_slave(shortname) do
-    {:ok, longname} = :ct_slave.start(shortname)
+    {:ok, hostname} = :inet.gethostname
+    {:ok, longname} = :slave.start(hostname, shortname)
     true     = :code.set_path(:code.get_path)               |> at(longname)
     {:ok, _} = :application.ensure_all_started(:raft_fleet) |> at(longname)
-    pid = Process.whereis(RaftFleet.Supervisor)             |> at(longname)
-    :erlang.group_leader(:erlang.group_leader, pid)
-    Supervisor.which_children(pid) |> Enum.each(fn {_, p, _, _} -> :erlang.group_leader(:erlang.group_leader, p) end)
   end
 
   def stop_slave(shortname) do
-    {:ok, _} = :ct_slave.stop(shortname)
+    {:ok, hostname} = :inet.gethostname
+    :ok = :slave.stop(:"#{shortname}@#{hostname}")
   end
 end
