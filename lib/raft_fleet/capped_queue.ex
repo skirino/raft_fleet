07c077685dd0ad9e82d810c9c5517fc915a0ef1c
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2016-10-13 17:10:51 +0900

    Refactoring: Use vanilla queue functions to suppress dialyzer warning

--- lib/raft_fleet/capped_queue.ex
+++ lib/raft_fleet/capped_queue.ex
@@ -1,7 +1,7 @@
 use Croma
 
 defmodule RaftFleet.CappedQueue do
-  use Croma.SubtypeOfTuple, elem_modules: [Croma.NonNegInteger, Croma.NonNegInteger, Croma.Tuple]
+  @type t :: {non_neg_integer, non_neg_integer, :queue.queue}
 
   defun new(max_length :: g[non_neg_integer]) :: t do
     {max_length, 0, :queue.new}
@@ -17,20 +17,10 @@ defmodule RaftFleet.CappedQueue do
     end
   end
 
-  defun filter({max, len, {l1, l2}} :: t, f :: (any -> boolean)) :: t do
-    {r1, n1} = filter_count(l1, f)
-    {r2, n2} = filter_count(l2, f)
-    q = {[], Enum.reverse(r2, r1)}
-    {max, len - n1 - n2, q}
-  end
-
-  defunp filter_count(l :: [any], f :: (any -> boolean)) :: {[any], non_neg_integer} do
-    Enum.reduce(l, {[], 0}, fn(e, {reversed, count}) ->
-      case f.(e) do
-        true  -> {[e | reversed], count}
-        false -> {reversed, count + 1}
-      end
-    end)
+  defun filter({max, _, q} :: t, f :: (any -> boolean)) :: t do
+    q2   = :queue.filter(f, q)
+    len2 = :queue.len(q2)
+    {max, len2, q2}
   end
 
   defun underlying_queue({_, _, q} :: t) :: :queue.queue, do: q
