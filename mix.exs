dd38bbd0adbe181aee2903409fb7df70476c1f5a
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2017-07-14 02:27:14 +0900

    v0.3.1

--- mix.exs
+++ mix.exs
@@ -6,10 +6,10 @@ defmodule RaftFleet.Mixfile do
   def project() do
     [
       app:             :raft_fleet,
-      version:         "0.3.0",
+      version:         "0.3.1",
       elixir:          "~> 1.3",
-      build_embedded:  Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded:  Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps:            deps(),
       description:     "A fleet of Raft consensus groups",
       package:         package(),
