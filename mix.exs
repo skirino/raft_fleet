375616bfc0f2804bff746811f9230081861f6563
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2017-08-14 15:30:51 +0900

    v0.4.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule RaftFleet.Mixfile do
   def project() do
     [
       app:             :raft_fleet,
-      version:         "0.4.0",
+      version:         "0.4.1",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
