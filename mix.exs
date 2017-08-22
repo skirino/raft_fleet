b3c70ae0146f300fac7c0c079b6cb809c4469373
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2017-08-22 21:38:08 +0900

    v0.4.2

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule RaftFleet.Mixfile do
   def project() do
     [
       app:             :raft_fleet,
-      version:         "0.4.1",
-      elixir:          "~> 1.3",
+      version:         "0.4.2",
+      elixir:          "~> 1.4",
       build_embedded:  Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps:            deps(),
@@ -28,7 +28,7 @@ defmodule RaftFleet.Mixfile do
 
   defp deps() do
     [
-      {:croma       , "~> 0.6"},
+      {:croma       , "~> 0.7"},
       {:rafted_value, "~> 0.4"},
       {:coverex     , "~> 1.4" , only: :test},
       {:dialyze     , "~> 0.2" , only: :dev },
